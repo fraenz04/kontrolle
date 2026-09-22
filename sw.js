70666d6705f57149292353c31e251856e51812e3
fraenz04 <anmeldung.fs@gmail.com>
2026-09-22 19:03:05 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vsk-v1';
+const CACHE = 'vsk-v4';
 const ASSETS = [
   './', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
